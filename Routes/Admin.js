3a898f6c4cdbd4a8c88a4fd7f8d560d4c0727aec
Charchitg <Charchitg07@gmail.com>
2022-06-30 00:09:35 +0530

    added sales routes , login , register get routes and authorization middleware

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -1,22 +1,39 @@
 const express = require('express');
-const { AddCustomer } = require('../Controllers/Create');
+const { AddSale } = require('../Controllers/AddSales');
+const { AddCustomer, getAddCustomer } = require('../Controllers/Create');
 const { DeleteCustomer } = require('../Controllers/Delete');
-const { Login } = require('../Controllers/Login');
-const { GetCustomers } = require('../Controllers/Read');
-const { Register } = require('../Controllers/Register');
+const { DeleteSale } = require('../Controllers/DeleteSales');
+const { Login, getLogin } = require('../Controllers/Login');
+const { GetCustomers, GetAllSales, GetCustomerSales } = require('../Controllers/Read');
+const { Register, getRegister } = require('../Controllers/Register');
 const { UpdateCustomer } = require('../Controllers/Update');
+const { UpdateSale } = require('../Controllers/UpdateSales');
+const { authorised } = require('../Jwt/Authorisation');
 const router = express.Router();
 
 // Admin signup/signin
-router.post('/register' , Register)
-router.post('/login' , Login)
+router.get('/register' , getRegister);
+router.post('/register' , Register);
 
+router.get('/login' ,getLogin);
+router.post('/login' , Login);
+router.post("/logout", authorised , async (req, res) => {
+    await res.clearCookie("accesstoken");
+    res.redirect('/login');
+  });
 
 // Admin powers
 router.get('/dashboard' , GetCustomers);
-router.post('/add' , AddCustomer);
-router.post('/update' , UpdateCustomer);
+router.get('/sales' , GetAllSales);
+router.get('/sales/:customer_email' , GetCustomerSales);
+
+router.get('/add' , authorised , getAddCustomer);
+router.post('/add' , authorised , AddCustomer);
+router.post('/update' , authorised , UpdateCustomer);
 router.post('/delete' , DeleteCustomer);
+router.post('/add_sale' , AddSale)
+router.post('/update_sale' , UpdateSale);
+router.post('/delete_sale' , DeleteSale);
 
 
 module.exports = router;
